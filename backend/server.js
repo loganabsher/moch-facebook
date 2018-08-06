d31b617fd476c07f933fed9e47f4b6b581ad2a15
Logan Absher <absherlogan@gmail.com>
2018-08-05 17:22:21 -0700

    server set up

--- backend/server.js
+++ backend/server.js
@@ -6,5 +6,26 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cors = require('cors');
 
 mongoose.Promise = require('bluebird');
+
+const errors = require('./lib/error-middleware.js');
+const authRoutes = require('./route/auth-route.js');
+
+dotenv.load();
+
+const app = express();
+// setting port to 8000 because 3000 is reserved for the front generateFindHash
+// NOTE: process.env needs to be removed for the final verson
+const PORT = process.env.PORT || 8000;
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN.split(' '),
+  credentials: true
+}));
+
+app.use(morgan('dev'));
+
+app.listen(PORT, () => debug(`backend running on PORT: ${PORT}`));
